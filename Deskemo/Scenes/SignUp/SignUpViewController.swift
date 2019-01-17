49b5e0ff626c3063516b185bd683c568272397d3
tri.ngo <ngominhtrint@gmail.com>
2019-01-17 11:09:51 +0800

    Update UI of SignUp screen.

--- Deskemo/Scenes/SignUp/SignUpViewController.swift
+++ Deskemo/Scenes/SignUp/SignUpViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxKeyboard
 
 class SignUpViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var ivAvatar: UIImageView!
     @IBOutlet weak var btnSignUp: UIButton!
+    @IBOutlet weak var tvBio: UITextView!
     
     var signUpViewModel: SignUpViewModel!
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
         bindViewModel()
+    }
+    
+    private func setupView() {
+        self.navigationItem.title = "SignUp"
+        scrollView.keyboardDismissMode = .interactive
+
+        btnSignUp.layer.cornerRadius = 6
+        btnSignUp.layer.borderWidth = 2
+        btnSignUp.layer.borderColor = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1).cgColor
+        
+        tvBio.layer.cornerRadius = 6
+        tvBio.layer.borderWidth = 1
+        tvBio.layer.borderColor = UIColor.gray.withAlphaComponent(0.3).cgColor
     }
     
     private func bindViewModel() {
@@ -28,5 +47,15 @@ class SignUpViewController: UIViewController {
         let input = SignUpViewModel.Input(signUpTrigger: btnSignUp.rx.tap.asDriver())
         
         let _ = signUpViewModel.transform(input: input)
+        
+        bindKeyboard()
+    }
+    
+    private func bindKeyboard() {
+        RxKeyboard.instance.visibleHeight
+            .drive(onNext: { keyboardVisibleHeight in
+                self.scrollView.contentInset.bottom = keyboardVisibleHeight
+            })
+            .disposed(by: disposeBag)
     }
 }
